1531a80c9db96036055241950c17d28b0ebcdf37
Tom Jammy <threcius@yahoo.com>
2023-10-03 15:19:10 +0800

    Add function to add two big integers

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _BigInt {
+    int *rep;
+    int n;
+    int d;
+} BigInt;
+
+BigInt add(BigInt a, BigInt b) {
+    int n = a.n;
+    int d = 0;
+    BigInt c;
+    c.rep = (int*)malloc(sizeof(int)*n);
+    for (int i = 0; i < n; i++) {
+        int sum = a.rep[i] + b.rep[i] + d;
+        d = (int)(sum / 10);
+        c.rep[i] = sum % 10;
+    }
+    c.n = n;
+    c.d = d;
+    return c;
+}
+
+int main() {
+    BigInt a;
+    int arep[] = {1, 1, 2, 1, 1, 2};
+    a.rep = (int*)&arep;
+    a.n = sizeof(arep) / sizeof(int);
+    a.d = 0;
+
+    BigInt b;
+    int brep[] = {1, 1, 2, 1, 1, 2};
+    b.rep = (int*)&brep;
+    b.n = sizeof(brep) / sizeof(int);
+    b.d = 0;
+
+    BigInt c = add(a, b);
+    for (int i = 0; i < c.n; i++) {
+        printf("%d", (int)c.rep[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
