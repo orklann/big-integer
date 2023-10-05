e752ce899b854773883564ea7f8bc805e0147735
Tom Jammy <threcius@yahoo.com>
2023-10-05 15:30:20 +0800

    Add substraction

--- big_integer.c
+++ big_integer.c
@@ -53,3 +53,42 @@ BigInt big_integer_add(BigInt a, BigInt b) {
     c.d = d;
     return c;
 }
+
+BigInt big_integer_substract(BigInt a, BigInt b) {
+    int n = a.n;
+    if (n < b.n) {
+        n = b.n;
+        int *rep = (int*)malloc(sizeof(int)*n);
+        for (int i = 0; i < n; i++) {
+            if (i < a.n) {
+                rep[i] = a.rep[i];
+            } else {
+                rep[i] = 0;
+            }
+        }
+        free(a.rep);
+        a.rep = rep;
+    } else {
+        int *rep = (int*)malloc(sizeof(int)*n);
+        for (int i = 0; i < n; i++) {
+            if (i < b.n) {
+                rep[i] = b.rep[i];
+            } else {
+                rep[i] = 0;
+            }
+        }
+        free(b.rep);
+        b.rep = rep;
+    }
+    int d = 0;
+    BigInt c;
+    c.rep = (int*)malloc(sizeof(int)*n);
+    for (int i = 0; i < n; i++) {
+        int sum = a.rep[i] - b.rep[i] + d;
+        d = (int)(sum / 10);
+        c.rep[i] = sum % 10;
+    }
+    c.n = n;
+    c.d = d;
+    return c;
+}
--- big_integer.h
+++ big_integer.h
@@ -7,3 +7,4 @@ typedef struct _BigInt {
 
 BigInt big_integer_create(char *s);
 BigInt big_integer_add(BigInt a, BigInt b);
+BigInt big_integer_substract(BigInt a, BigInt b);
